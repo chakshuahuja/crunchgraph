6d1c83df78ee4dfec7c40a112525fb1254bdf7c7
Siddhant Sanyam <siddhant3s@gmail.com>
2017-01-15 04:23:51 -0800

    Added neo4j

--- src/routes/data/index.js
+++ src/routes/data/index.js
@@ -1,11 +1,31 @@
 import express from 'express';
+import neo4j from 'neo4j';
 
 const router = express.Router();
 
+const db = new neo4j.GraphDatabase({
+  url: 'http://192.168.0.100:7474',
+  auth: { username: 'neo4j', password: 'pear' },
+});
+
+const query = 'MATCH (o:Organization {company_name:{company_name}})' +
+  '-[r:ACQUIRED]->(a) return r,a';
+
 router.get('/', (req, res) => {
-  res.json({
-    status: 'Okay',
-  });
+  db.cypher(
+    {
+      query,
+      params: { company_name: 'Facebook' },
+    },
+    (err, results) => {
+      if (err) throw err;
+      // results.forEach(r => console.log(r.p.relationships[0]));
+      res.json({
+        status: 'Okay',
+        results,
+      });
+    },
+  );
 });
 
 export default router;
